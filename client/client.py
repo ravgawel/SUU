862faf9b0ba5b1e6beba7789d3e8b49e0bf24527
lukasz.kowalski <lukowalski97@gmail.com>
2021-06-10 20:49:11 +0200

    added async tcp server

--- client/client.py
+++ client/client.py
@@ -1,28 +1,78 @@
+import argparse
+import sched
 import socket
+import threading
+import time
+from _thread import *
 
-server_ip = "127.0.0.1"
-udp_server_port = 2405
-tcp_server_port = 2405
+parser = argparse.ArgumentParser(description='Simple Tcp/Udp client to send data to corresponding server')
+
+parser.add_argument('--address', help='Server address. Default is 0.0.0.0', default="127.0.0.1")
+
+parser.add_argument('--udp_port', help='Server udp port. Default is 2405', default=2405)
+
+parser.add_argument('--tcp_port', help='Server tcp port. Default is 2405', default=2405)
+
+parser.add_argument('--peak_tcp', help='How many tcp packets are meant to be sent per second. Default is 5',
+                    default=5)
+
+parser.add_argument('--peak_udp', help='How many udp packets are meant to be sent per second. Default is 50',
+                    default=50)
+
+args = parser.parse_args()
+
+server_ip = args.address
+udp_server_port = args.udp_port
+tcp_server_port = args.tcp_port
+
+tcp_peak_per_sec = args.peak_tcp
+udp_peak_per_sec = args.peak_udp
 
 
-def send_udp_msg(message):
+def send_udp(udp_socket, udp_scheduler, start_time):
+    udp_scheduler.enter(1, 1, send_udp,
+                        kwargs={'udp_socket': udp_socket, 'udp_scheduler': udp_scheduler, 'start_time': start_time})
+
+    print(time.time())
+    for i in range(udp_peak_per_sec):
+        udp_socket.sendto(str.encode(f'udp {start_time}'), (server_ip, udp_server_port))
+
+        # msg_from_server = udp_socket.recvfrom(128)
+    print(time.time())
+    udp_scheduler.run()
+
+
+def start_udp_packets(start_time):
     udp_client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-    udp_client_socket.sendto(str.encode(message), (server_ip, udp_server_port))
-
-    msg_from_server = udp_client_socket.recvfrom(1024)
-    print("Received from the UDP server: " + msg_from_server[0].decode())
+    udp_scheduler = sched.scheduler(time.time, time.sleep)
+    send_udp(udp_client_socket, udp_scheduler, start_time)
 
 
-def send_tcp_msg(message):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((server_ip, tcp_server_port))
-    s.send(message.encode())
+def tcp_client(ip, port, message):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((ip, port))
+    sock.sendall(message.encode())
+    result = sock.recv(1024)
+    print(f'message {message} \t result {result}')
+    sock.close()
 
-    msg_from_server = s.recv(1024)
-    print('Received from the TCP server:', msg_from_server.decode())
-    s.close()
+
+def start_tcp_client():
+    while True:
+        thread_list = []
+        for i in range(tcp_peak_per_sec):
+            msg = "Thread #{}, clnt time {}".format(i, time.time())
+            client_thread = threading.Thread(
+                target=tcp_client, args=(server_ip, tcp_server_port, msg))
+            thread_list.append(client_thread)
+            client_thread.start()
+
+        [x.join() for x in thread_list]
+        print(f'iteration finished {time.time()}')
 
 
 if __name__ == '__main__':
-    send_udp_msg("UDP message")
-    send_tcp_msg("TCP message")
+    start_new_thread(start_udp_packets, (time.time(),))
+    start_new_thread(start_tcp_client, ())
+    while True:
+        pass
--- server/server.py
+++ server/server.py
@@ -1,5 +1,17 @@
+import argparse
+import asyncore
 import socket
 from _thread import *
+
+parser = argparse.ArgumentParser(description='Simple Tcp/Udp server to get data on specific ports and address')
+
+parser.add_argument('--address', help='Ip Address to listen to. Default is 0.0.0.0', default='0.0.0.0')
+
+parser.add_argument('--udp_port', help='Port to listen to for udp data. Default is 2405', default=2405)
+
+parser.add_argument('--tcp_port', help='Port to listen to for tcp data. Default is 2405', default=2405)
+
+args = parser.parse_args()
 
 
 def udp_server(ip, port):
@@ -14,25 +26,33 @@ def udp_server(ip, port):
         udp_server_socket.sendto(message, address)
 
 
-def tcp_server(ip, port):
-    tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp_server_socket.bind((ip, port))
-    tcp_server_socket.listen(5)
-    print('TCP server ' + ip + ':' + str(port) + ' up and listening')
+class TcpServer(asyncore.dispatcher):
 
-    while True:
-        connection, address = tcp_server_socket.accept()
-        message = connection.recv(1024)
+    def __init__(self, host, port):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_reuse_addr()
+        self.bind((host, port))
+        self.listen(5)
 
-        print('TCP(' + str(address[0]) + ':' + str(address[1]) + '): ' + message.decode())
-        connection.send(message)
-        connection.close()
+    def handle_accept(self):
+        socket, address = self.accept()
+        TcpEchoHandler(socket)
+
+
+class TcpEchoHandler(asyncore.dispatcher_with_send):
+
+    def handle_read(self):
+        msg = self.recv(512)
+        print(msg.decode())
+        self.close()
 
 
 if __name__ == '__main__':
-    ip_address = "0.0.0.0"
-    udp_port = 2405
-    tcp_port = 2405
+    ip_address = args.address
+    udp_port = args.udp_port
+    tcp_port = args.tcp_port
 
     start_new_thread(udp_server, (ip_address, udp_port))
-    tcp_server(ip_address, tcp_port)
+    server = TcpServer(ip_address, tcp_port)
+    asyncore.loop()
